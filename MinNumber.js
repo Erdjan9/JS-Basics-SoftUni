40c0dfd9bf82f1ed333573ea2d8698b4e7c832c3
Erdjan <erdjan.asipov11@gmail.com>
2023-05-07 00:32:57 +0300

    Create MinNumber.js

---
+++ MinNumber.js
@@ -0,0 +1,17 @@
+function MaxNum(input) {
+    let index = 1,
+      difference = 0;
+    let MaxNum = Number(input[0]);
+    let inputItems = input[index];
+    while (inputItems !== "Stop") {
+      difference = Number(input[index]);
+  
+      if (MaxNum > difference) {
+        MaxNum = Number(input[index]);
+      }
+      inputItems = input[index];
+      index++;
+    }
+    console.log(MaxNum);
+  }
+  MaxNum(["100", "99", "80", "70", "Stop"]);
